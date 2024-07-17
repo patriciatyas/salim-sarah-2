592a68f85ed889770f856013554190b4f33c8564
patriciatyas <patriciatyas11@gmail.com>
2024-07-18 03:42:40 +0700

    delete react icon and change title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import React from "react";
+import React, { useEffect } from "react";
 import CopyButton from "./components/CopyButton";
 import { useInView } from "react-intersection-observer";
 
