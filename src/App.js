4c2d196b5019bfccd7ba691f4fd5659aabee4df0
patriciatyas <patriciatyas11@gmail.com>
2024-07-18 16:26:47 +0700

    fix audio 2

--- src/App.js
+++ src/App.js
@@ -20,6 +20,12 @@ function App() {
   return (
     <div className="relative items-center justify-center min-h-screen">
 
+    <audio unmuted autoplay loop>
+      <source src={`${process.env.PUBLIC_URL}/up.ogg`} type="audio/ogg" />
+      <source src={`${process.env.PUBLIC_URL}/up.mp3`} type="audio/mpeg" />
+      Your browser does not support the audio element.
+    </audio>
+
       <FadeInSection>
         <div className="hidden md:block fixed-left"></div>
       </FadeInSection>
